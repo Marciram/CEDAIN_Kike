af29e9affd63d9ba0bf892fd64c908ed1dd02ac0
Marciram <A01562798@tec.mx>
2023-08-09 18:49:55 -0600

    hola mundo

---
+++ app/index.js
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native';
+const Home = () => {
+ return (
+ <View>
+ <Text>Home</Text>
+ <Text>Maria del Cielo Ramirez Zavala A01562798
+    Hobbie: Pintar
+    Kain y avel.
+    Alejandro Bustos A01568802 
+    Hobbie: Futball.
+    Animales o dioses.
+    Ricardo Aguirre A01562775.
+    Hobbie: Videojuegos
+    habitos atomicos
+ </Text>
+ </View>
+ )
+}
+export default Home;
